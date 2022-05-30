09d07343b55af6486ac913f2106e358f855fbfe6
Essa Sheikh <essaubaid0@gmail.com>
2022-05-30 20:05:18 +0500

    stripe dependencies

--- src/cmp/Cart/Pay.jsx
+++ src/cmp/Cart/Pay.jsx
@@ -1,11 +1,11 @@
 import StripeCheckout from 'react-stripe-checkout';
-import { useState,useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const KEY = "pk_test_51L4nldDp99vLXs5cXWP7XCyYKjt1NkcWH0TxsMqP7PTix6XGyypGdEaM68jXWEsMw3a3ZOKgRvGUnrnOjTQXR8NZ00el7SAeFK"
 const Pay = () => {
 
-    const [stripeToken,setStripeToken] = useState(null);
+    const [stripeToken, setStripeToken] = useState(null);
 
 
 
@@ -13,59 +13,59 @@ const Pay = () => {
         setStripeToken(token);
     };
 
-    useEffect(()=>{
+    useEffect(() => {
         const makeRequest = async () => {
-            try{
+            try {
                 const res = await axios.post("http://localhost:5000/api/checkout/payment", {
-                    tokenID:stripeToken.id,
-                    amount:2000,
+                    tokenID: stripeToken.id,
+                    amount: 2000,
                 }
                 );
                 console.log(res.data);
-            }catch(err){
+            } catch (err) {
                 console.log(err)
             }
-             };
-             stripeToken && makeRequest()
-    },[stripeToken])
+        };
+        stripeToken && makeRequest()
+    }, [stripeToken])
 
-    return(
+    return (
         <div
-        style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-        }}
+            style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+            }}
         >
             {stripeToken ? (
                 <span>Processing. please wait...</span>
             ) : (
-        <StripeCheckout 
-        name="Thrifty."
-        billingAddress
-        shippingAddress
-        description="Your total is Rs5000"
-        amount={2000}
-        token= {onToken}
-        stripeKey={KEY}
-        >
-        <button
-        style={{
-            border: "none",
-            borderRadius:5,
-            padding:"8px",
-            width:120,
-            backgroundColor: "black",
-            color: "white",
-            fontWeight: "600",
-            cursor: "pointer",
-        }}
-        >
-            Stripe Pay
-            </button>
-            </StripeCheckout> 
+                <StripeCheckout
+                    name="Thrifty."
+                    billingAddress
+                    shippingAddress
+                    description="Your total is Rs5000"
+                    amount={2000}
+                    token={onToken}
+                    stripeKey={KEY}
+                >
+                    <button
+                        style={{
+                            border: "none",
+                            borderRadius: 5,
+                            padding: "8px",
+                            width: 120,
+                            backgroundColor: "black",
+                            color: "white",
+                            fontWeight: "600",
+                            cursor: "pointer",
+                        }}
+                    >
+                        Stripe Pay
+                    </button>
+                </StripeCheckout>
             )}
-            </div>
+        </div>
     );
 };
 
